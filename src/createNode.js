a0362aa910ea003aedc3a6111eabda706e9f2901
celery-kotone <t11080hi@sfc.keio.ac.jp>
2012-08-13 03:37:02 +0900

    Minor modifications

--- src/createNode.js
+++ src/createNode.js
@@ -1,4 +1,4 @@
-createNode = function(geometry, material, scene){
+Node = function(geometry, material, scene){
     var thisNode = this;
 
     thisNode.mesh      = new THREE.Mesh(geometry, material);
@@ -7,13 +7,19 @@ createNode = function(geometry, material, scene){
     scene.add(thisNode.mesh);
 
     thisNode.child      = [];
-    thisNode.childId    = [];
-    thisNode.clicked    = 0;
-    thisNode.isChild    = 0;
+    thisNode.clicked    = null;
+    thisNode.move       = null;
+    thisNode.distance   = 0;
     thisNode.theta      = 0;
     thisNode.generation = 0;
+    thisNode.radius     = 800;
+    thisNode.ready      = 1;
 
     thisNode.update = function(){
+
+	if(!thisNode.ready){
+	    return;
+	}
 
 	//RECURSIVELY UPDATE CHILD NODES
 	if(thisNode.child.length > 0){
@@ -24,29 +30,48 @@ createNode = function(geometry, material, scene){
 
 	//POSITION CHILD NODES
 	if(thisNode.child.length > 0){
-	    for(var i in thisNode.child){
-		thisNode.child[i].mesh.position.x = 300;
+	    thisNode.theta = 2 * Math.PI / thisNode.child.length;
+	    for(var i = 0; i < thisNode.child.length; i++){
+		thisNode.x2 = Math.pow(thisNode.child[i].mesh.position.x - thisNode.mesh.position.x, 2);
+		thisNode.y2 = Math.pow(thisNode.child[i].mesh.position.y - thisNode.mesh.position.y, 2);
+		thisNode.z2 = Math.pow(thisNode.child[i].mesh.position.z - thisNode.mesh.position.z, 2);
+		thisNode.distance = Math.sqrt(thisNode.x2 + thisNode.y2 + thisNode.z2);
+		document.getElementById('rotation').innerHTML = thisNode.distance + " " + thisNode.child[i].ready;
+		if(thisNode.distance < thisNode.radius){
+		    thisNode.child[i].mesh.position.x += Math.cos(thisNode.theta * i) * Math.sqrt(thisNode.radius - thisNode.distance);
+		    thisNode.child[i].mesh.position.z += Math.sin(thisNode.theta * i) * Math.sqrt(thisNode.radius - thisNode.distance);
+		}else{
+		    thisNode.child[i].ready = 1;
+		}
 	    }
 	}
 
-	//CHECK COLLISION
-	renderer.domElement.addEventListener('mousedown', thisNode.mouse, false);
+	//CHECK MOUSECLICK
+	renderer.domElement.addEventListener('click', thisNode.mouseClick, false);
+
+	//CHECK MOUSEOVER
+	renderer.domElement.addEventListener('mousemove', thisNode.mouseOver, false);
 
 	//ROTATE MESH
 	thisNode.mesh.rotation.y = ((thisNode.mesh.rotation.y.toFixed(2) * 100) % (Math.PI.toFixed(2) * 100) + 1) / 100;
 
-	//RESCALE MESH
+	//CREATE/REMOVE CHILD NODES
 	if(thisNode.clicked){
-	    if(thisNode.child.length < 1){
-		thisNode.createChild(1, geometry, material, scene);
+	    if(thisNode.child.length == 0){
+		thisNode.createChild(6, geometry, material, scene);
 	    }
+	}else{
+	    thisNode.removeChild();
+	}
+
+	//RESIZE NODE
+	if(thisNode.move || thisNode.clicked){
 	    if(thisNode.mesh.scale.x < 2){
 		thisNode.mesh.scale.x += 0.1;
 		thisNode.mesh.scale.y += 0.1;
 		thisNode.mesh.scale.z += 0.1;
 	    }
 	}else{
-	    thisNode.removeChild();
 	    if(thisNode.mesh.scale.x > 1){
 		thisNode.mesh.scale.x -= 0.1;
 		thisNode.mesh.scale.y -= 0.1;
@@ -55,7 +80,7 @@ createNode = function(geometry, material, scene){
 	}
     };
 
-    thisNode.mouse = function(e){
+    thisNode.mouseClick = function(e){
 	thisNode.mouse_x =   ((e.pageX-e.target.offsetParent.offsetLeft) / renderer.domElement.width)  * 2 - 1;
 	thisNode.mouse_y = - ((e.pageY-e.target.offsetParent.offsetTop) / renderer.domElement.height) * 2 + 1;
 	thisNode.vector = new THREE.Vector3(thisNode.mouse_x, thisNode.mouse_y, 0.5);
@@ -72,16 +97,41 @@ createNode = function(geometry, material, scene){
 	}
     };
 
+    thisNode.mouseOver = function(e){
+        thisNode.mouse_x =   ((e.pageX-e.target.offsetParent.offsetLeft) / renderer.domElement.width)  * 2 - 1;
+        thisNode.mouse_y = - ((e.pageY-e.target.offsetParent.offsetTop) / renderer.domElement.height) * 2 + 1;
+        thisNode.vector = new THREE.Vector3(thisNode.mouse_x, thisNode.mouse_y, 0.5);
+        thisNode.projector.unprojectVector(thisNode.vector, camera);
+
+        thisNode.ray = new THREE.Ray(camera.position, thisNode.vector.subSelf(camera.position).normalize());
+        thisNode.obj = thisNode.ray.intersectObjects([thisNode.mesh]);
+        if(thisNode.obj.length > 0){
+	    thisNode.move = 1;
+        }else{
+	    thisNode.move = 0;
+	}
+    };
+
     thisNode.createChild = function(max, geometry, material, scene){
 	for(var i = 0; i < max; i++){
-	    thisNode.child[i] = new createNode(geometry, material, scene);
-	    thisNode.childId[i] = thisNode.child[i].mesh.id;
+	    thisNode.child[i] = new Node(geometry, material, scene);
+	    thisNode.child[i].ready = 0;
+	    //thisNode.child[i].mesh.position = thisNode.mesh.position;
 	}
     };
 
     thisNode.removeChild = function(){
-	for(var i = 0; i < thisNode.child; i++){
-	    scene.remove(thisNode.childId[i]);
+	for(var i = 0; i < thisNode.child.length; i++){
+	    if(thisNode.child[i].mesh.scale.x > 0){
+		thisNode.child[i].mesh.scale.x -= 0.1;
+		thisNode.child[i].mesh.scale.y -= 0.1;
+		thisNode.child[i].mesh.scale.z -= 0.1;
+	    }else{
+		scene.remove(thisNode.child[i].mesh);
+		if(i == thisNode.child.length - 1){
+		    thisNode.child.length = 0;
+		}
+	    }
 	}
     };
 };
--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function init() {
     scene = new THREE.Scene();
  
     camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
-    camera.position.z = 500;
+    camera.position.z = 1500;
     scene.add(camera);
  
     div_canvas = document.getElementById('canvas');
@@ -35,13 +35,10 @@ function initScene() {
     var geometry = new THREE.SphereGeometry(100, 10, 10);
     var material = new THREE.MeshBasicMaterial({wireframe: true, color: 0x6060ff});
     material.transparent = true;
-    node = new createNode(geometry, material, scene);
+    node = new Node(geometry, material, scene);
 }
 
 function render(){
-    if(node.child.length > 0){
-	document.getElementById('rotation').innerHTML = node.childId[0];
-    }
     node.update();
     controls.update();
     renderer.render(scene, camera);
