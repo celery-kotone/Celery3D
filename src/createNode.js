bccb8b9ca65a46f2a41dcff1b927bdf7d21bf0e6
celery-kotone <t11080hi@sfc.keio.ac.jp>
2012-08-14 15:09:10 +0900

    Change Y axis on click

--- src/createNode.js
+++ src/createNode.js
@@ -9,12 +9,14 @@ Node = function(geometry, material, scene){
     thisNode.child      = [];
     thisNode.clicked    = null;
     thisNode.move       = null;
+    thisNode.lastClick  = null;
     thisNode.distance   = 0;
     thisNode.theta      = 0;
     thisNode.generation = 0;
     thisNode.radius     = 800;
     thisNode.ready      = 1;
     thisNode.hasChild   = 0;
+    thisNode.cameraPos  = 0;
 
     thisNode.update = function(){
 
@@ -26,13 +28,17 @@ Node = function(geometry, material, scene){
 	}
 
 	//POSITION CHILD NODES
-	if(thisNode.child.length > 0){
+	if(thisNode.hasChild){
 	    thisNode.theta = 2 * Math.PI / thisNode.child.length;
 	    for(var i = 0; i < thisNode.child.length; i++){
 		thisNode.x2 = Math.pow(thisNode.child[i].mesh.position.x - thisNode.mesh.position.x, 2);
 		thisNode.y2 = Math.pow(thisNode.child[i].mesh.position.y - thisNode.mesh.position.y, 2);
 		thisNode.z2 = Math.pow(thisNode.child[i].mesh.position.z - thisNode.mesh.position.z, 2);
 		thisNode.distance = Math.sqrt(thisNode.x2 + thisNode.y2 + thisNode.z2);
+		if(thisNode.child[i].mesh.position.y < (thisNode.child[i].generation - 1) * 500){
+		    thisNode.child[i].mesh.position.y += Math.sqrt((thisNode.child[i].generation - 1) * 500 - thisNode.child[i].mesh.position.y);
+		    thisNode.mesh.position.y = thisNode.child[i].mesh.position.y;
+		}
 		if(thisNode.distance < thisNode.radius){
 		    thisNode.child[i].mesh.position.x += Math.cos(thisNode.theta * i) * Math.sqrt(thisNode.radius - thisNode.distance);
 		    thisNode.child[i].mesh.position.z += Math.sin(thisNode.theta * i) * Math.sqrt(thisNode.radius - thisNode.distance);
@@ -40,13 +46,11 @@ Node = function(geometry, material, scene){
 		    thisNode.child[i].ready = 1;
 		}
 	    }
+	}else{
+	    if(thisNode.mesh.position.y > (thisNode.generation - 1)  * 500 && thisNode.generation > 0){
+		thisNode.mesh.position.y -= Math.sqrt(thisNode.mesh.position.y - (thisNode.generation - 1) * 500);
+	    }
 	}
-
-	//CHECK MOUSECLICK
-	renderer.domElement.addEventListener('click', thisNode.mouseClick, false);
-
-	//CHECK MOUSEOVER
-	renderer.domElement.addEventListener('mousemove', thisNode.mouseOver, false);
 
 	//ROTATE MESH
 	thisNode.mesh.rotation.y = ((thisNode.mesh.rotation.y.toFixed(2) * 100) % (Math.PI.toFixed(2) * 100) + 1) / 100;
@@ -76,6 +80,21 @@ Node = function(geometry, material, scene){
 		thisNode.mesh.scale.z -= (Math.pow(thisNode.mesh.scale.z - 1, 1/2)) * 0.07;
 	    }
 	}
+
+	//MOVE CAMERA
+	if(thisNode.lastClick){
+	    if(controls.target != thisNode.position){
+		controls.target.x += (thisNode.mesh.position.x - controls.target.x) / 10;
+		controls.target.y += (thisNode.mesh.position.y - controls.target.y) / 10;
+		controls.target.z += (thisNode.mesh.position.z - controls.target.z) / 10;
+	    }
+	}
+
+	//CHECK MOUSECLICK
+	renderer.domElement.addEventListener('click', thisNode.mouseClick, false);
+
+	//CHECK MOUSEOVER
+	renderer.domElement.addEventListener('mousemove', thisNode.mouseOver, false);
     };
 
     thisNode.mouseClick = function(e){
@@ -87,11 +106,14 @@ Node = function(geometry, material, scene){
 	thisNode.ray = new THREE.Ray(camera.position, thisNode.vector.subSelf(camera.position).normalize());
 	thisNode.obj = thisNode.ray.intersectObjects([thisNode.mesh]);
 	if(thisNode.obj.length > 0){
+	    thisNode.lastClick = 1;
 	    if(thisNode.clicked){
 		thisNode.clicked = 0;
 	    }else{
 		thisNode.clicked = 1;
 	    }
+	}else{
+	    thisNode.lastClick = 0;
 	}
     };
 
@@ -114,6 +136,8 @@ Node = function(geometry, material, scene){
 	for(var i = 0; i < max; i++){
 	    thisNode.child[i] = new Node(geometry, material, scene);
 	    thisNode.child[i].ready = 0;
+	    thisNode.child[i].radius = 500;
+	    thisNode.child[i].generation = thisNode.generation + 1;
 	    thisNode.child[i].mesh.position.x = thisNode.mesh.position.x;
 	    thisNode.child[i].mesh.position.y = thisNode.mesh.position.y;
 	    thisNode.child[i].mesh.position.z = thisNode.mesh.position.z;
